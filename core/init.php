2813119fdc8f5bc231dd484b2d75a589f739cbf9
xrayhunter <3002612+xrayhunter@users.noreply.github.com>
2018-09-22 17:00:49 -0700

    Cleanup

--- core/init.php
+++ core/init.php
@@ -3,21 +3,8 @@
 
     // Load normal Libraries:
     $GLOBALS['extensions'] = array();
-    spl_autoload_register(function($class) {
-        
-        /*$str = explode('_', $class);
-        foreach ($_GLOBALS['extensions'] as $extension)
-        {
-            if (in_array($str[count($str) - 1], $extension['name']))
-            {
-                require_once 'lib/extensions/' . (isset($extension['dir']) ? $extension['dir'] . '/' : '') . $class . '.php';
-                return;
-            }
-        }
-
-        require_once "lib/" . $class . ".php";
-        */
-
+    spl_autoload_register(function($class) 
+    {
         if (file_exists('lib/' . $class . '.php'))
             require_once "lib/" . $class . ".php";
         else
